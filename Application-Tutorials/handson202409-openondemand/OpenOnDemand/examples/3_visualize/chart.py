c8a70e96adbc0c3c3569c70c3d507ca7cbf16c06
Tomoya Tanjo <ttanjo@gmail.com>
2024-09-11 08:39:58 +0900

    Restructure examples

---
+++ Application-Tutorials/handson202409-openondemand/OpenOnDemand/examples/3_visualize/chart.py
@@ -0,0 +1,20 @@
+# ジョブ結果を可視化する
+
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+
+result_dir = '/home/ooduser/results'
+result_files = [os.path.join(result_dir, f) for f in os.listdir(result_dir) if f.startswith('result_')]
+
+data = [np.load(f) for f in result_files]
+
+plt.figure(figsize=(10, 6))
+for i, d in enumerate(data):
+    plt.plot(d, label=f'Task {i}')
+plt.xlabel('Index')
+plt.ylabel('Value')
+plt.title('Results from SLURM tasks')
+plt.legend()
+plt.show()
+
